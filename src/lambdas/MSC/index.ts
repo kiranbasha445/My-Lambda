c642de03f4ff0bc3505f5d407cdfb0cb78cea0ab
kiranbasha445 <kiranbasha445@gmail.com>
2025-02-15 11:28:15 +0530

    staginglatesttt

--- src/lambdas/MSC/index.ts
+++ src/lambdas/MSC/index.ts
@@ -25,7 +25,7 @@ export const handler: Handler = async (event: LambdaEvent) => {
       })
     };
   } catch (error) {
-    console.error('Error process ing event:', error);
+    console.error('Error processing event:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({
